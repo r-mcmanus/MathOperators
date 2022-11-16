3fc4b23ca49069e8275206dfa6f6f80981946bfe
mcman <mcmanus452@gmail.com>
2022-11-16 09:03:54 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Addition
+sum = 1 + 1
+
+# Subtraction
+diff = 2 - 1
+
+# Multiplication
+product = 2 * 2
+
+# Division (any time this operator is used it always produces a float)
+quotient = 10 / 5
+
+#Modulus
+remainder = 10 % 6
+
+print(sum) #2
+print(diff) #1
+print(product) #4
+print(quotient) #2.0
+print(remainder) #4
+print(remainder + quotient / product * (diff - sum)) #3.5
